43276991e0c0bf8e61cc86019bd43c36e4481db0
Rodrigo-Daw <63906770+Rodrigo-Daw@users.noreply.github.com>
2023-05-13 20:52:34 +0200

    php actualizado

--- certamenes.php
+++ certamenes.php
@@ -7,14 +7,14 @@ $conexion = new PDO($server,$user,$pw,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 class Certamenes{}
 
 function listarCertamenes($connection){
 
     $listado = array();
-    $sql = "SELECT * FROM certamenesMusica ORDER BY fecha";
+    $sql = "SELECT * FROM certamenesmusica ORDER BY fecha";
     $query = $connection->prepare($sql);
 	$query->execute();
 
@@ -26,6 +26,57 @@ function listarCertamenes($connection){
 		$certamen->lugar = $row['lugar'];
 
 		$listado[] = $certamen;
+	}
+
+    return $listado;
+}
+
+class CertamenesCelebrados{}
+
+function certamenesCelebrados($connection){
+
+    $listado = array();
+    $sql = "SELECT * FROM certamenes_celebrados ORDER BY fecha DESC";
+    $query = $connection->prepare($sql);
+	$query->execute();
+
+    while ($row = $query->fetch(PDO::FETCH_NAMED)){
+		$certamen = new CertamenesCelebrados();
+		$certamen->id = $row['id'];
+		$certamen->nombre_certamen = $row['nombre_certamen'];
+		$certamen->fecha = $row['fecha'];
+		$certamen->lugar = $row['lugar'];
+
+		$listado[] = $certamen;
+	}
+
+    return $listado;
+}
+
+
+class SoloBandas{}
+
+function listarSoloBandas($connection){
+
+    $listado = array();
+    $sql = "SELECT * FROM bandasdemusica";
+    $query = $connection->prepare($sql);
+	$query->execute();
+
+    while ($row = $query->fetch(PDO::FETCH_NAMED)){
+		$soloBandas = new SoloBandas();
+		$soloBandas->id = $row['id'];
+		$soloBandas->nombre_banda = $row['nombre_banda'];
+		$soloBandas->nombre_director = $row['nombre_director'];
+		$soloBandas->numero_musicos = $row['numero_musicos'];
+        $soloBandas->localidad = $row['localidad'];
+        $soloBandas->provincia = $row['provincia'];
+        $soloBandas->codigo_postal= $row['codigo_postal'];
+        $soloBandas->telefono = $row['telefono'];
+        $soloBandas->correo_electronico = $row['correo_electronico'];
+        $soloBandas->clave = $row['clave'];
+
+		$listado[] = $soloBandas;
 	}
 
     return $listado;
@@ -74,6 +125,29 @@ function insertarBanda($connection, $jsonBanda){
     ]);
 }
 
+function insertarCertamen($connection, $jsonCertamen){
+
+    $sql = "INSERT INTO certamenesmusica (nombre_certamen, fecha, lugar) VALUES (?,?,?)";
+    $sentencia = $connection->prepare($sql);
+    $resultado = $sentencia->execute([$jsonCertamen->nombre_certamen, $jsonCertamen->fecha, $jsonCertamen->lugar]);
+    
+    echo json_encode([
+        "resultado" => $resultado,
+    ]);
+}
+
+
+function inscripcion($connection, $banda_id, $certamen_id){
+
+    $sql = "INSERT INTO banda_certamen (banda_id, certamen_id, fecha, hora) VALUES (?,?,CURRENT_DATE(), CURTIME())";
+    $sentencia = $connection->prepare($sql);
+    $resultado = $sentencia->execute([$banda_id, $certamen_id]);
+
+    echo json_encode([
+        "resultado" => $resultado,
+    ]);
+}
+
 class Login{}
 
 function accesoBanda($connection, $jsonAcceso){
@@ -96,32 +170,127 @@ function accesoBanda($connection, $jsonAcceso){
     }
 }
 
-class Banda{}
+class Juez{}
 
-function getBanda($connection, $id){
+function accesoJuez($connection, $jsonJuez){
 
-    $listado = array();
-    $sql = "SELECT * FROM bandasdemusica WHERE id = ?";
+    $sql = "SELECT * FROM jueces";
     $query = $connection->prepare($sql);
 	$query->execute();
 
     while($row = $query->fetch(PDO::FETCH_NAMED)){
 
-		$banda = new Banda();
+		$login = new Juez();
+        $login->id = $row['id'];
+        $login->correo_electronico = $row['correo_electronico'];
+        $login->clave = $row['clave'];
+
+        if($jsonJuez->correo_electronico == $login->correo_electronico && $jsonJuez->clave == $login->clave){
+            $usuario =  $login->id;
+            return $usuario;
+        }
+    }
+}
+
+class Banda{}
+
+function getBanda($connection, $id){
+    $sql = "SELECT * FROM bandasdemusica WHERE id = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bindParam(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($row) {
+        $banda = new Banda();
         $banda->id = $row['id'];
-		$banda->nombre_banda = $row['nombre_banda'];
-		$banda->nombre_director = $row['nombre_director'];
-		$banda->numero_musicos = $row['numero_musicos'];
+        $banda->nombre_banda = $row['nombre_banda'];
+        $banda->nombre_director = $row['nombre_director'];
+        $banda->numero_musicos = $row['numero_musicos'];
         $banda->localidad = $row['localidad'];
         $banda->provincia = $row['provincia'];
         $banda->codigo_postal = $row['codigo_postal'];
         $banda->telefono = $row['telefono'];
         $banda->correo_electronico = $row['correo_electronico'];
-
-        $listado[] = $banda;
+        return $banda;
+    } else {
+        return null;
     }
+}
 
+class Nota{}
+
+function getNota($connection, $id){
+
+    $listado = array();
+    $sql = "SELECT * FROM bandasdemusica INNER JOIN banda_cert_celebrado ON bandasdemusica.id=banda_cert_celebrado.banda_id 
+        INNER JOIN certamenes_celebrados ON banda_cert_celebrado.certamen_id=certamenes_celebrados.id 
+        WHERE certamenes_celebrados.id = ? ORDER BY nota";
+    $stmt = $connection->prepare($sql);
+    $stmt->bindParam(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_NAMED)){
+            $certamen = new Nota();
+            $certamen->id_ambos = $row['id_ambos'];
+            $certamen->nombre_banda = $row['nombre_banda'];
+            $certamen->nombre_certamen = $row['nombre_certamen'];
+            $certamen->nota = $row['nota'];
+            
+            $listado[] = $certamen;
+    }
+  
     return $listado;
+}
+
+
+function editarBanda($connection, $id, $bandaeditada){
+
+    $sql = "UPDATE bandasdemusica SET nombre_banda=?, nombre_director=?, numero_musicos=?, localidad=?, provincia=?, codigo_postal=?, telefono=?, correo_electronico=? WHERE id=?";
+    $stmt = $connection->prepare($sql);
+    
+    
+    $stmt->bindParam(9,$id, PDO::PARAM_INT);
+
+    $stmt->bindParam(1, $bandaeditada->nombre_banda, PDO::PARAM_STR);
+    $stmt->bindParam(2, $bandaeditada->nombre_director, PDO::PARAM_STR);
+    $stmt->bindParam(3, $bandaeditada->numero_musicos, PDO::PARAM_STR);
+    $stmt->bindParam(4, $bandaeditada->localidad, PDO::PARAM_STR);
+    $stmt->bindParam(5, $bandaeditada->provincia, PDO::PARAM_STR);
+    $stmt->bindParam(6, $bandaeditada->codigo_postal, PDO::PARAM_STR);
+    $stmt->bindParam(7, $bandaeditada->telefono, PDO::PARAM_STR);
+    $stmt->bindParam(8, $bandaeditada->correo_electronico, PDO::PARAM_STR);
+    
+    $resultado = $stmt->execute();
+
+    echo json_encode([
+        "resultado" => $resultado,
+    ]);
+}
+
+function puntuarBanda($connection, $id, $nota){
+
+    $sql = "UPDATE banda_cert_celebrado SET nota=? WHERE id=?";
+    $stmt = $connection->prepare($sql);
+
+    $stmt->bindParam(2,$id, PDO::PARAM_INT);
+    $stmt->bindParam(1, $nota, PDO::PARAM_STR);
+
+    $resultado = $stmt->execute();
+
+    echo json_encode([
+        "resultado" => $resultado,
+    ]);
+}
+
+function eliminarBanda($connection, $id){
+    
+    $sentencia = "DELETE FROM bandasdemusica WHERE id = ?";
+    $stmt = $connection->prepare($sentencia);
+    $stmt->bindParam(1,$id, PDO::PARAM_INT);
+    $resultado = $stmt->execute();
+
+    echo json_encode([
+        "resultado" => $resultado,
+    ]);
 }
 
 
@@ -129,6 +298,12 @@ function getBanda($connection, $id){
 if(isset($_GET['opcion'])){
     if($_GET['opcion'] == 'proximosCertamenes'){
         echo json_encode( listarCertamenes($conexion), JSON_UNESCAPED_UNICODE );
+    }
+    else if($_GET['opcion'] == 'certamenesCelebrados'){
+        echo json_encode( certamenesCelebrados($conexion), JSON_UNESCAPED_UNICODE );
+    }
+    else if($_GET['opcion'] == 'soloBandas'){
+        echo json_encode( listarSoloBandas($conexion), JSON_UNESCAPED_UNICODE );
     }
     else if($_GET['opcion'] == 'bandas'){
         echo json_encode( listarBandas($conexion), JSON_UNESCAPED_UNICODE );
@@ -146,16 +321,57 @@ if(isset($_GET['opcion'])){
         echo json_encode( accesoBanda($conexion, $jsonAcceso), JSON_UNESCAPED_UNICODE );
 
     }
-    else if($_GET['opcion'] == 'crud' & $_GET['id']){
+    else if($_GET['opcion'] == 'juez'){
 
-        if(isset($_GET['id']) & !empty($_GET['id'])){
-            $id = $_GET['id'];
-        }else{
-            echo 'No hay id';
-            var_dump('No hay id');
-        }
+        $jsonJuez = json_decode(file_get_contents("php://input"));
+
+        echo json_encode( accesoJuez($conexion, $jsonJuez), JSON_UNESCAPED_UNICODE );
+    }
+    else if($_GET['opcion'] == 'crud' & isset($_GET['id'])){
+
+        $id = $_GET['id'];
 
         echo json_encode( getBanda($conexion, $id), JSON_UNESCAPED_UNICODE );
+    } 
+    else if($_GET['opcion'] == 'editar' && isset($_GET['id'])){
+
+        $id = $_GET['id'];
+
+        $bandaeditada = json_decode(file_get_contents("php://input"));
+        echo json_encode( editarBanda($conexion, $id, $bandaeditada), JSON_UNESCAPED_UNICODE );
+    }
+    else if($_GET['opcion'] == 'eliminar' && isset($_GET['id'])){
+
+        $id = $_GET['id'];
+        
+        echo json_encode( eliminarBanda($conexion, $id), JSON_UNESCAPED_UNICODE );
+    } 
+    else if($_GET['opcion'] == 'listarPorCertamen' && isset($_GET['id'])){
+
+        $id = $_GET['id'];
+        $nota = json_decode(file_get_contents("php://input"));
+
+        echo json_encode( puntuarBanda($conexion, $id, $nota), JSON_UNESCAPED_UNICODE );
+    }
+    else if($_GET['opcion'] == 'inscribirse' && isset($_GET['id'])){
+
+        $banda_id = $_GET['id'];
+
+        $certamen_id = json_decode(file_get_contents("php://input"));
+
+        echo json_encode( inscripcion($conexion, $banda_id, $certamen_id), JSON_UNESCAPED_UNICODE );
+    } 
+    else if($_GET['opcion'] == 'nuevoCertamen'){
+
+        $jsonCertamen = json_decode(file_get_contents("php://input"));
+
+        echo json_encode( insertarCertamen($conexion, $jsonCertamen), JSON_UNESCAPED_UNICODE );
+    }
+    else if($_GET['opcion'] == 'puntuacion' & isset($_GET['id'])){
+
+        $id = $_GET['id'];
+
+        echo json_encode( getNota($conexion, $id), JSON_UNESCAPED_UNICODE );
     } 
     else{
         echo "Hay errores";
